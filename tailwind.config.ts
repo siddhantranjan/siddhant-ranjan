27f3833d8bcdc13bbb2cf32a743a90d8233b199c
Siddhant <siddhant@ncompass.inc>
2024-02-05 00:23:44 +0530

    refactor: updated tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      height: {
+        '128': '32rem',
+        '160': '40rem',
+        '200': '50rem',
       },
-    },
+      width: {
+        '128': '32rem',
+        '160': '40rem',
+        '200': '50rem',
+      },
+      colors: {
+        "primary": "#F5F5F5",
+        "secondary": "#E5E4E2",
+        "tertiary": "#FF91A4",
+        "lighter": "#f7fafc",
+        "light": "#cbd5e0",
+        "normal": "#a0aec0",
+        "dark": "#4a5568",
+        "darker": "#1a202c"
+      },
+      display: ['group-hover']
+    }
   },
   plugins: [],
-};
-export default config;
+}
+export default config
