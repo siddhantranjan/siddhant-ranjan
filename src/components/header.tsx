ffb5b0d851ab34e0ed047be73f1e9651746168ba
Siddhant <siddhant@ncompass.inc>
2024-02-05 18:59:15 +0530

    feature: added logic for sticky header and scroll to particular section

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,47 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 const Header = () => {
+    const [isSticky, setIsSticky] = useState(false);
+    useEffect(() => {
+      const handleScroll = () => {
+        if (scrollY > 200) {
+          setIsSticky(true);
+        } else {
+          setIsSticky(false);
+        }
+      }
+  
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    useEffect(() => {
+        const handleClick = (e: any) => {
+          e.preventDefault();
+          const targetId = e.target.getAttribute('href');
+          const targetElement = document.querySelector(targetId);
+         
+          window.scrollTo({
+            top: targetElement.offsetTop - 70, 
+            behavior: 'smooth'
+          });
+    
+        }
+        const anchors = document.querySelectorAll('a');
+        anchors.forEach(a => a.addEventListener('click', handleClick));
+        return () => anchors.forEach(a => a.removeEventListener('click', handleClick));
+    
+      });
     return (
         <header className="w-full bg-transparent text-white h-16 fixed top-0">
-            <div className="flex items-center justify-around h-full">
+            <div className={`main_header flex items-center justify-around h-full ${isSticky ? "sticky_menu" : ""}`}>
                 <div className="">
-                    <a className="text-4xl">Siddhant</a>
+                    <a className="text-4xl font-extrabold tracking-wider text-tertiary">Siddhant</a>
                 </div>
 
                 <div className="h-full">
-                    <button type="button" className="hidden" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span className=""></span>
-                    </button>
-
                     <nav id="navbar" className="flex justify-center h-full">
                         <ul className="flex items-center justify-center space-x-6 text-base">
                             <li><a href="#home">Home</a></li>
@@ -18,7 +49,6 @@ const Header = () => {
                             <li><a href="#service">Service</a></li>
                             <li><a href="#resume">Resume</a></li>
                             <li><a href="#portfolio">Portfolio</a></li>
-                            <li><a href="#blog">Blog</a></li>
                             <li><a href="#contact">Contact</a></li>
                         </ul>
                     </nav>
