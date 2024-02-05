fbc4c4a336c9633dce98c747f8f697fae3d203f4
Siddhant <siddhant@ncompass.inc>
2024-02-05 18:56:51 +0530

    feature: added about, service, resume, portfolio and contact section and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+import AboutSection from "@/components/about-section";
+import ContactSection from "@/components/contact-section";
+import Footer from "@/components/footer";
 import Header from "@/components/header";
 import IntroSection from "@/components/intro-section";
+import PortfolioSection from "@/components/portfolio-section";
+import ResumeSection from "@/components/resume-section";
+import ServiceSection from "@/components/service-section";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="text-lighter">
       <Header />
       <IntroSection />
+      <div className="bg-darker">
+        <AboutSection />
+        <ServiceSection />
+        <ResumeSection />
+        <PortfolioSection />
+        <ContactSection />
+      </div>
+      <Footer />
     </main>
   );
 }
